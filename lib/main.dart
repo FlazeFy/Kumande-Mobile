4a8cad6912bf08179d13663a141e0184ec23f352
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-05-09 17:17:55 +0700

    [BE] show firebase notif / FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:kumande/Components/Navbars/bottom.dart';
 import 'package:kumande/Pages/Landings/LoginPage/index.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool shouldUseFirestoreEmulator = false;
+final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +18,53 @@ Future<void> main() async {
   if (shouldUseFirestoreEmulator) {
     FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
   }
+
+  String token = await FirebaseMessaging.instance.getToken();
+  print('FCM Registration Token: $token');
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('launch_background');
+
+  final InitializationSettings initializationSettings =
+      const InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+  });
+
+  Future<void> msgHandler(RemoteMessage message) async {
+    String title = message.notification?.title ?? '';
+    String body = message.notification?.body ?? '';
+    String payload = message.data['data'];
+
+    AndroidNotificationDetails androidNotificationDetails =
+        const AndroidNotificationDetails(
+            'channel_id', 'channel_name', 'channel_description',
+            priority: Priority.high,
+            importance: Importance.high,
+            ticker: 'ticker');
+
+    NotificationDetails platformChannelSpecifics = NotificationDetails(
+      android: androidNotificationDetails,
+    );
+    Future<void> fcmBgHandler(RemoteMessage message) async {
+      await Firebase.initializeApp();
+      msgHandler(message);
+    }
+
+    FirebaseMessaging.onBackgroundMessage(fcmBgHandler);
+
+    await flutterLocalNotificationsPlugin
+        .show(0, title, body, platformChannelSpecifics, payload: payload);
+  }
+
+  FirebaseMessaging.onMessage.listen(msgHandler);
 
   final prefs = await SharedPreferences.getInstance();
   if (prefs.containsKey("token_key")) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,9 @@ import cloud_firestore
 import firebase_analytics
 import firebase_auth
 import firebase_core
+import firebase_messaging
 import firebase_storage
+import flutter_local_notifications
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
